3f4ee025012785f484a9eee1d0b4ebd43846e749
amazingrise <8315221+AmazingRise@users.noreply.github.com>
2020-03-22 23:39:15 +0800

    Add support for nested menu.

--- static/js/journal.js
+++ static/js/journal.js
@@ -72,3 +72,20 @@ app = new Vue({
 });
 
 new SmoothScroll('a#globalBackToTop');
+
+Vue.component('parent',{
+    props:['name','type'],
+    data: function () {
+        return {
+            isHidden: true
+        }
+    },
+    template: `
+    <div class="a-block" :class="type" v-on:click="isHidden = !isHidden">
+        {{ name }}
+        <div class="nav-link-subitem" v-if="!isHidden">
+            <slot></slot>
+        </div>
+    </div>
+    `
+});
